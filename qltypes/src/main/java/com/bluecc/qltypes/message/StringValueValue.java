d8f7f299bb4887a72e36bb07a0af86968755c5c2
Samlet Wu <xiaofei.wu@gmail.com>
2022-06-21 20:55:25 +0800

    add tree-node-proto

---
+++ qltypes/src/main/java/com/bluecc/qltypes/message/StringValueValue.java
@@ -0,0 +1,15 @@
+package com.bluecc.qltypes.message;
+
+import com.bluecc.qltypes.pubs.*;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import java.util.List;
+
+@Data
+@Accessors(chain = true)
+public class StringValueValue {
+    String value;
+}
+
+
+
